4da39fa5a4fb76793c9c61683ca335ec8f7ff53b
Alex <sanyanesmiyanov92@gmail.com>
2021-03-23 21:41:14 +0200

    create style.scss

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-// const { listenerCount } = require("process");
 const searchForm = document.querySelector('.header__form');
 const cityValue = document.querySelector('.header__form__input');
 const cityName = document.querySelector('.header__output__container__top__left h2');
 const cardBody = document.querySelector('.header__output__container');
-const sitiesListContainer = document.querySelector('.sitiesListContainer');
+const citiesListContainer = document.querySelector('.citiesContainer');
 const errorListContainer = document.querySelector('.error');
-//const timeImage = document.querySelector('.card-top img');
 const cardInfo = document.querySelector('.header__output');
-const dayNightIcon = document.querySelector('.header__container');
+const dayNightBg = document.querySelector('.header__container');
+const dayNightSearchButton = document.querySelector('.header__form__submit');
+// const dayNightListButton = document.querySelector('.citiesContainer__list__item:hover');
+const footerYear = document.querySelector('.footer__year')
+const today = new Date();
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 
+//transform kelvin to celcium
 const spitOutCelcius = (kelvin) => {
     celcius = Math.round(kelvin - 273.15);
     return celcius;
@@ -18,17 +24,29 @@ const isDayTime = (icon) => {
     else { return false }
 }
 
+//add date Now
+yearFooter = () => {
+  footerYear.innerHTML = `${today.getFullYear()}`
+}
+
+//add day or night background
 const dayNightSwitch = () => {
   const hours = new Date().getHours()
-  const isDayTime = hours > 6 && hours < 20
+  const isDayTime = hours > 7 && hours < 19
   if (isDayTime) {
-    dayNightIcon.style.backgroundImage = "url('img/day-bg.png')";
+    dayNightBg.style.backgroundImage = "url('img/day-bg.png')";
+    dayNightSearchButton.style.backgroundImage = "url('img/search.png')";
+    dayNightSearchButton.style.backgroundColor = "#90CAF9";
+    // dayNightListButton.style.backgroundColor = "#90CAF9";
   }else{
-    dayNightIcon.style.backgroundImage = "url('img/night-bg.png')";
+    dayNightBg.style.backgroundImage = "url('img/night-bg.png')";
+    dayNightSearchButton.style.backgroundImage = "url('img/search.png')";
+    dayNightSearchButton.style.backgroundColor = "#2A344B";
+    // dayNightListButton.style.backgroundColor = "#2A344B";
   }
 }
 
-
+//Add the city weather details to output
 updateWeatherApp = (city) => {
     console.log(city);
     const imageName = city.weather[0].icon;
@@ -50,7 +68,11 @@ updateWeatherApp = (city) => {
         </div>
         <div class="header__output__container__top__right">
           <h2>${city.name}</h2>
-          <p></p>
+          <div class="header__output__container__top__right__span">
+                  <span>${today.getDate()}</span>
+                  <span>${monthNames[today.getMonth()]}</span>
+                  <span>${today.getFullYear()}</span>
+                </div>
         </div>
       </div>
 
@@ -69,28 +91,30 @@ updateWeatherApp = (city) => {
   
     cardInfo.classList.remove('d-none');
 }
+
 const myFunction = (city) => {
   console.log(city);
     requestCity(city)
         .then((data) => {
             updateWeatherApp(data);
-            sitiesListContainer.classList.add('d-none');
+            citiesListContainer.classList.add('d-none');
         })
         .catch((error) => { console.log(error) })  
 }
 
+//Get the list of cities
 listCities = (data) => {
   const cities = data.list.map((el) => el.name )
-  sitiesListContainer.innerHTML = `
-  <ul class="sities-list">
-  ${cities.map(el => `<button class="sities-item" id="${el}" onclick="myFunction(this.id)">${el}</button>`).join("")}
+  citiesListContainer.innerHTML = `
+  <ul class="citiesContainer__list">
+  ${cities.map(el => `<button class="citiesContainer__list__item" id="${el}" onclick="myFunction(this.id)">${el}</button>`).join("")}
   </ul>
   `
   errorListContainer.innerHTML = `
   <p>City not found, please try to change your search query</p>
   `
   if (cities.length) {
-    sitiesListContainer.classList.remove('d-none');
+    citiesListContainer.classList.remove('d-none');
   }
   else {
     errorListContainer.classList.remove('d-none');
@@ -120,7 +144,7 @@ function inputFocus() {
   errorListContainer.classList.add('d-none');
 }
 
-//accordion
+//Questions
 var acc = document.getElementsByClassName("main__accordion-block__item");
 var i;
 
@@ -136,4 +160,5 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-dayNightSwitch()
+dayNightSwitch();
+yearFooter();
